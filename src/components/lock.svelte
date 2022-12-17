4aa9ea470f63598c3ce0b3ac054bc3a240aa9469
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-17 17:04:39 +0800

    Change int type to number

--- src/components/lock.svelte
+++ src/components/lock.svelte
@@ -1,20 +1,24 @@
-<script lang='ts'>
-let secret: String;
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let secret: String;
+	let open: boolean;
 
-function randomNumber(max: int, min: int): int{
-	return Math.random() * (max-min) + min;
-}
-
-function generateSecret(length: int): String{
-	let result: String = "";
-	let numbers: int[] = Array.from(Array(length).keys());
-	while(numbers.length > 0){
-		let randomIndex: int = randomNumber(0, numbers.length)
-		result += String(numbers[randomIndex]);
-		numbers.splice(randomIndex, 1);
+	function randomNumber(max: number, min: number): number {
+		return Math.random() * (max - min) + min;
 	}
-	return result;
-}
-</script>
 
-<img src={}/>
+	function generateSecret(length: number): String {
+		let result: String = '';
+		let numbers: number[] = Array.from(Array(length).keys());
+		while (numbers.length > 0) {
+			let randomIndex: number = randomNumber(0, numbers.length);
+			result += String(numbers[randomIndex]);
+			numbers.splice(randomIndex, 1);
+		}
+		return result;
+	}
+
+	onMount(() => {
+		secret = generateSecret(4);
+	});
+</script>
