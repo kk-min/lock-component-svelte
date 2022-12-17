ee803e33391a0cf2636ec992374fe50ef56fa45d
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-17 16:40:16 +0800

    Add files, add generateSecret() function

---
+++ src/components/lock.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+let secret: String;
+
+function randomNumber(max: int, min: int): int{
+	return Math.random() * (max-min) + min;
+}
+
+function generateSecret(length: int): String{
+	let result: String = "";
+	let numbers: int[] = Array.from(Array(length).keys());
+	while(numbers.length > 0){
+		let randomIndex: int = randomNumber(0, numbers.length)
+		result += String(numbers[randomIndex]);
+		numbers.splice(randomIndex, 1);
+	}
+	return result;
+}
+</script>
+
+<img src={}/>
