824a249abe659dd0074411c78283f4ebae7e5d3f
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-18 08:49:55 +0800

    Add onClick function to Unlocker.svelte

---
+++ src/components/Unlocker.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { userInput } from "src/lib/stores/UserStore";
+	let locked: Boolean = true;
+	let unlockers: String[] = ['0', '1', '2', '3']
+
+	function onClick(value: String){
+		if(locked){
+			return;
+		}
+		userInput.update(prev => prev+value);
+	} 
+</script>
+
+{#each unlockers as input (input)}
+	<button on:click={(input) => onClick(input)}>input</button>
+{/each}
