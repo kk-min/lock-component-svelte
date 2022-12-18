b4d7686c51a4f325ffb9673eec0d3d5eafac03f1
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-18 10:46:11 +0800

    Add stlying to unlocker buttons

--- src/components/Unlocker.svelte
+++ src/components/Unlocker.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
-	import { userInput } from "../lib/stores/UserStore";
+	import { userInput } from '../lib/stores/UserStore';
 	let locked: boolean = false;
-	let unlockers: String[] = ['0', '1', '2', '3']
-	
-	$: if($userInput.length === 4){
-		locked = true;
-	} 
+	let unlockers: String[] = ['0', '1', '2', '3'];
 
-	function onClick(value: String){
-		if(locked){
+	$: if ($userInput.length === 4) {
+		locked = true;
+	}
+
+	function onClick(value: String) {
+		if (locked) {
 			return;
 		}
-		userInput.update(prev => prev+value);
+		userInput.update((prev) => prev + value);
 		console.log($userInput);
-	} 
+	}
 </script>
 
-{#each unlockers as code (code)}
-	<button on:click={() => onClick(code)} disabled={locked}>{code}</button>
-{/each}
+<div class="unlocker-container">
+	{#each unlockers as code (code)}
+		<button class="unlocker" on:click={() => onClick(code)} disabled={locked}>{code}</button>
+	{/each}
+</div>
+
+<style>
+	.unlocker-container {
+		display: flex;
+		gap: 1rem;
+	}
+
+	.unlocker {
+		border: 1px solid black;
+		padding: 10px;
+		font-size: 1.5rem;
+		background-color: white;
+	}
+</style>
