3997f07b4d6c9c75a9c6ecda208b770153e36c11
mink0003 <76023265+kk-min@users.noreply.github.com>
2022-12-18 08:00:33 +0800

    Add Unlocker.svelte file

--- src/components/lock.svelte
+++ src/components/lock.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	let secret: String;
-	let open: boolean;
-
-	function randomNumber(max: number, min: number): number {
-		return Math.random() * (max - min) + min;
-	}
-
-	function generateSecret(length: number): String {
-		let result: String = '';
-		let numbers: number[] = Array.from(Array(length).keys());
-		while (numbers.length > 0) {
-			let randomIndex: number = randomNumber(0, numbers.length);
-			result += String(numbers[randomIndex]);
-			numbers.splice(randomIndex, 1);
-		}
-		return result;
-	}
-
-	onMount(() => {
-		secret = generateSecret(4);
-	});
+	import {secret} from '../lib/stores/SecretStore';
+	import {userInput} from '../lib/stores/UserStore'
+	let open: boolean = ($secret == $userInput);
 </script>
+
+{#if open}
+
+{:else}
+
+{/if}
